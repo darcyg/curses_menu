ebb8ff0a7629d3baf0580128166c8ebf0c9e7d45
dhpradeep <dhpradeep25@gmail.com>
2018-05-12 15:38:22 +0545

    Initialization

---
+++ README.md
@@ -0,0 +1,32 @@
+### About
+
+This is curses menu maker.
+It can make menu.
+
+### How To
+```bash
+$ pip install pyqt5
+$ pip install curses
+```
+
+### Example
+
+```python
+from curses_menu import *
+
+# write your items in a list
+items = ['home', 'contact', 'exit']
+choose = menu(items, x_co=5, y_co=5, box=False)
+# x_co, y_co and box are optionals
+
+if choose == 1:
+	gotohome()
+elif choose == 2:
+	gotocontact()
+elif choose == 3:
+	exit()
+```
+
+### Screen Shots
+
+![screen-shot](https://user-images.githubusercontent.com/22606403/39956062-17fcdfaa-55f9-11e8-817f-e5823c43edfe.PNG)
---
+++ curse_menu.py
@@ -0,0 +1,39 @@
+import curses
+
+stdscr = curses.initscr()
+curses.cbreak()
+curses.noecho()
+stdscr.keypad(True)
+
+def menu(items, x_co=5,y_co=5, box=False):
+	stdscr.refresh()
+	if box == False:
+		pass
+	else:
+		pass
+		# Do later in here...
+	try:
+		place = 0
+		c = 0
+		while(1):
+			stdscr.clear()
+			xco = x_co
+			yco = y_co
+			if c == curses.KEY_DOWN:
+				place += 1
+				if place == len(items): place = 0
+			if c == curses.KEY_UP:
+				place -= 1
+				if place < 0: place = len(items) - 1
+			if c == curses.KEY_ENTER or c == 10 or c == 13:
+				return (place + 1)
+
+			for n,menu in enumerate(items):
+				if place == n:
+					stdscr.addstr(yco, xco, '->'+items[n])
+				else:
+					stdscr.addstr(yco, xco, '  '+items[n])
+				yco += 1
+			c = stdscr.getch()
+	except Exception as e:
+			print(e.message)
